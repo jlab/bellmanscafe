15a4ad0a125cdcba0cd7fdac6a3eec45656fe9b7
lmtz13 <leon.merlin.orlando.metzger@kooperation-jlu.thm.de>
2022-07-11 13:33:23 +0200

    Made path in shell script relative instead of absolute.

--- pull_and_copy_ADP_collection.sh
+++ pull_and_copy_ADP_collection.sh
@@ -6,6 +6,6 @@ echo "Copying .gap and .hh files to the folder ~/bellmanscafe/"
 cp ./*.gap ../bellmanscafe/
 cp ./*.hh ../bellmanscafe/
 echo "Success."
-echo "Update was performed at: " >> $HOME/bellmanscafe/gapfiles_updates.log
-date >> $HOME/bellmanscafe/gapfiles_updates.log
-echo "---------" >> $HOME/bellmanscafe/gapfiles_updates.log
+echo "Update was performed at: " >> ../bellmanscafe/gapfiles_updates.log
+date >> ../bellmanscafe/gapfiles_updates.log
+echo "---------" >> ../bellmanscafe/gapfiles_updates.log
